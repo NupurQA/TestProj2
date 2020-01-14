9f3f0396923b4ebc08a0dc4f98628752ba207927
Chauhan Nupur <Chauhan Nupur@NDE-15037263.bcg.com>
2020-01-14 17:48:28 +0530

    second commit

--- Practice/src/CollFreq.java
+++ Practice/src/CollFreq.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-//author : nupur
+//author : Nupur
 public class CollFreq {
 	 public static void main(String[] args) {
 	
